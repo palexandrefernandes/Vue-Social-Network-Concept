5855874f4175ed8b321866a0c5a5f1d28bc96436
palf1997 <palexandrefernandes@ipvc.pt>
2019-12-20 00:56:00 +0000

    no message

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -5,23 +5,48 @@ const User = require('../models/user');
 const secret = 'ThisAintNoSecret';
 const saltRounds = 10;
 
-function signup(req, res, next) {
-    let userData = res.body;
-    userData.password = encryptPassword(userData.password);
+async function signup(req, res, next) {
+    let userData = req.body;
+    userData.password = await encryptPassword(userData.password);
+    console.log(userData.password);
     User.query().insert(userData)
-        .then(res => {
+        .then(user => {
             res.status(200).json({success: 'User created'});
         })
         .catch(err => {
-            next();
+            next(err);
         });
 }
 
-async function encryptPassword(password){
-    return await bcrypt.hash(password, saltRounds);
+async function getToken(req, res, next) {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let user = await User.query().findOne({email: email});
+
+    console.log(user);
+
+    if(user && await comparePasswords(password, user.password)){
+        res.status(200).json({success: {token_type: 'Bearer', token: generateToken(user.id, user.name, 24)}});
+    }
+    else {
+        res.status(400).json({error: 'Credentials dont match'});
+    }
+}
+
+function generateToken(id, name, durationInHours) {
+    return jwt.sign({id: id}, secret, {subject: name, issuer: 'ShoutOut', expiresIn: 60 * 60 * durationInHours});
+}
+
+function encryptPassword(password){
+    return bcrypt.hash(password, saltRounds).then(hash => hash);
+}
+
+function comparePasswords(password, hash){
+    return bcrypt.compare(password, hash).then(res => res).catch(err => false);
 }
 
 module.exports = {
     signup,
-
+    getToken
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const controller = require('../controllers/auth-controller');
 const router = express.Router();
+const m = require('../middlewares/authenticate');
+
+router.use(m);
 
 router.post('/api/auth/signup', controller.signup);
+
+router.post('/api/auth/token', controller.getToken);
+
 
 module.exports = router;
